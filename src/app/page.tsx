6867e8a3c5a325bbbb8cd220390190d3ef93b756
Daycross <dev.sapucaia@gmail.com>
2025-05-22 19:17:55 -0300

    fix: try to fix import build error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 
+import Header        from "./components/Header";
+import BannerMain    from "./components/BannerMain";
+import NewsContainer from "./components/NewsContainer";
+import CatalogSpots  from "./components/Catalogo";
+import Footer        from "./components/Footer";
+
 import styles from "./page.module.css";
-import Header from "./components/Header";
-import BannerMain from "./components/BannerMain";
-import Footer from "./components/Footer";
-import NewsContainer from "./components/NewsContainer"
-import CatalogSpots from "./components/Catalogo"
 
 export default function Home() {
   return (
