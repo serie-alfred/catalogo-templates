e28240acaf792fb4022ae4c20d58cd9a69157f87
kaua-yanase <kaua.yanase@seriea.digital>
2026-05-27 17:11:00 -0300

    styles: fix styles

--- src/components/templates/common/novoTemplate/Footer/index.tsx
+++ src/components/templates/common/novoTemplate/Footer/index.tsx
@@ -295,7 +295,7 @@ const FooterNovo = () => {
                 required
               />
               <button type="submit" className={styles.nlBtn}>
-                Cadastrar
+                <p>Cadastrar</p>
                 <svg viewBox="0 0 24 24">
                   <path d="M5 12h14" />
                   <path d="m13 6 6 6-6 6" />
